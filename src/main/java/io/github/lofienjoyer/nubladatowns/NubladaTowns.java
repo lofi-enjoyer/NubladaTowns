a0cf4f2456ba1a9744f904ea50c93502c40568cc
lofienjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2024-08-25 13:54:45 +0100

    Fix config file not being properly reloaded

--- src/main/java/io/github/lofienjoyer/nubladatowns/NubladaTowns.java
+++ src/main/java/io/github/lofienjoyer/nubladatowns/NubladaTowns.java
@@ -69,6 +69,7 @@ public final class NubladaTowns extends JavaPlugin {
     }
 
     public void reloadPlugin() {
+        reloadConfig();
         this.configValues = new ConfigValues();
         powerManager.reloadConfig();
         this.localizationManager.reloadConfig();
